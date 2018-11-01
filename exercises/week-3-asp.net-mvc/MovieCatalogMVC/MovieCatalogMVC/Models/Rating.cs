4d9a98f04a3dce4e3b34a8afdd3b7b01728cfe13
Hasan Gyulyustan <hasan@fmi-plovdiv.org>
2018-11-01 21:27:43 +0200

    Added week 3 exercises in asp net mvc.

---
+++ exercises/week-3-asp.net-mvc/MovieCatalogMVC/MovieCatalogMVC/Models/Rating.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MovieCatalogMVC.Models
+{
+    public class Rating
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Rating")]
+        [Required]
+        [StringLength(5)]
+        public string RatingValue { get; set; }
+
+        public virtual ICollection<Movie> Movies { get; set; }
+    }
+}
